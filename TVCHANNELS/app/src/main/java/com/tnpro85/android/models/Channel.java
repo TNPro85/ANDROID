d7247b0793242a431676223e1631c0bbab55bc59
tuannh <mrhoangtuan@gmail.com>
2015-05-20 01:14:13 +0700

    Add Channels project

---
+++ TVCHANNELS/app/src/main/java/com/tnpro85/android/models/Channel.java
@@ -0,0 +1,29 @@
+package com.tnpro85.android.models;
+
+import java.util.Comparator;
+
+/**
+ * Created by TUAN on 20/05/2015.
+ */
+public abstract class Channel implements Comparable<Channel> {
+
+    protected int cNum;
+    protected String cName;
+    protected int cLogo;
+
+    public Channel() {
+        cName = "";
+    }
+    public Channel(int num, String name) {
+        super();
+        this.cNum = num;
+        this.cName = name;
+    }
+
+    @Override
+    public int compareTo(Channel another) {
+        if(this.cNum < another.cNum) return -1;
+        if(this.cNum == another.cNum) return 0;
+        return 1;
+    }
+}
