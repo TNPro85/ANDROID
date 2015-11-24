0c7138e006e71e3811208896010841ed69004e6c
tuannh2 <tuannh2@VNG.COM.VN>
2015-11-24 11:55:28 +0700

    - Update draw gradient by code

---
+++ DEMO/TNSoftDemo/app/src/main/java/com/zing/demo/uicontrol/GradientView.java
@@ -0,0 +1,42 @@
+package com.zing.demo.uicontrol;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by CPU10819-local on 24/11/2015.
+ */
+public class GradientView extends RelativeLayout {
+
+    public GradientView(Context context) {
+        super(context);
+        init();
+    }
+
+    public GradientView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public GradientView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        this.setWillNotDraw(false);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint p = new Paint();
+        p.setShader(new LinearGradient(0, 0, 0, getWidth(), 0x00000000, 0x60000000, Shader.TileMode.CLAMP));
+        canvas.drawPaint(p);
+    }
+}
