c0765b6b0f2aaada17fc59aabb11faa4e37c0027
tuannh2 <tuannh2@VNG.COM.VN>
2016-01-07 18:26:09 +0700

    - Update ContentProvider

--- TVCHANNELS/app/src/main/java/com/tnpro85/mytvchannels/db/CP.java
+++ TVCHANNELS/app/src/main/java/com/tnpro85/mytvchannels/db/CP.java
@@ -4,8 +4,14 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import com.tnpro85.mytvchannels.models.Device;
 
 public class CP extends ContentProvider {
     private static final String AUTHORITY = "com.tnpro.tvchannels";
@@ -32,28 +38,98 @@ public class CP extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        int uriType = mURIMatcher.match(uri);
+        switch (uriType) {
+            case CODE_DEVICE:
+                queryBuilder.setTables(DBConst.TABLE.TBL_DEVICE_NAME);
+                break;
+            case CODE_DEVICE_ID:
+                queryBuilder.appendWhere(DBConst.TABLE.TBL_DEVICE_COL.COLUMN_NAME_ID + "=" + uri.getLastPathSegment());
+                break;
+        }
+
+        Cursor cursor = queryBuilder.query(DBHelper.getInstance().getWritableDatabase(),
+                projection, selection, selectionArgs, null, null, sortOrder);
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
         return null;
     }
 
     @Nullable
     @Override
-    public String getType(Uri uri) {
-        return null;
-    }
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+        SQLiteDatabase db = DBHelper.getInstance().getWritableDatabase();
+        long id = 0;
+        int uriType = mURIMatcher.match(uri);
+        switch (uriType) {
+            case CODE_DEVICE:
+                id = db.insertWithOnConflict(DBConst.TABLE.TBL_DEVICE_NAME, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+                break;
+        }
 
-    @Nullable
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        if(getContext() != null && getContext().getContentResolver() != null)
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        return Uri.parse(uri + "/" + id);
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        SQLiteDatabase db = DBHelper.getInstance().getWritableDatabase();
+        int uriType = mURIMatcher.match(uri);
+        int rowsDeleted = 0;
+        switch (uriType) {
+            case CODE_DEVICE:
+                rowsDeleted = db.delete(DBConst.TABLE.TBL_DEVICE_NAME, selection, selectionArgs);
+                break;
+            case CODE_DEVICE_ID:
+                String deviceId = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    rowsDeleted = db.delete(DBConst.TABLE.TBL_DEVICE_NAME, DBConst.TABLE.TBL_DEVICE_COL.COLUMN_NAME_ID + "=" + deviceId, null);
+                } else {
+                    rowsDeleted = db.delete(DBConst.TABLE.TBL_DEVICE_NAME, DBConst.TABLE.TBL_DEVICE_COL.COLUMN_NAME_ID + "=" + deviceId + " and "
+                            + selection, selectionArgs);
+                }
+                break;
+        }
+
+        if(getContext() != null && getContext().getContentResolver() != null)
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        return rowsDeleted;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        SQLiteDatabase db = DBHelper.getInstance().getWritableDatabase();
+        int uriType = mURIMatcher.match(uri);
+        int rowsUpdated = 0;
+        switch (uriType) {
+            case CODE_DEVICE:
+                rowsUpdated = db.update(DBConst.TABLE.TBL_DEVICE_NAME, values, selection, selectionArgs);
+                break;
+            case CODE_DEVICE_ID:
+                String deviceId = uri.getLastPathSegment();
+                if (TextUtils.isEmpty(selection)) {
+                    rowsUpdated = db.update(DBConst.TABLE.TBL_DEVICE_NAME,
+                            values, DBConst.TABLE.TBL_DEVICE_COL.COLUMN_NAME_ID + "=" + deviceId, null);
+                } else {
+                    rowsUpdated = db.update(DBConst.TABLE.TBL_DEVICE_NAME,
+                            values, DBConst.TABLE.TBL_DEVICE_COL.COLUMN_NAME_ID + "=" + deviceId + " and "
+                            + selection, selectionArgs);
+                }
+                break;
+        }
+
+        if(getContext() != null && getContext().getContentResolver() != null)
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        return rowsUpdated;
     }
 }
